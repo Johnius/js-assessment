3adbd6b24b15e9fc8f2fbb3c539ba023280267c5
Johnius <me@johnius.ru>
2016-06-27 12:04:25 -0400

    Objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj)
   },
 
   alterObjects: function(constructor, greeting) {
-
+    return constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+    return Object.keys(obj).map(key => `${key}: ${obj[key]}`)
+    // Or we can use for-in with hasOwnProperty method but
+    //   it takes more cognitive tension so we will on current version
   }
 };
