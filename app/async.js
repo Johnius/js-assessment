bec61a6ad005d9c0fe5a3d47c935bafd101f7ced
Johnius <me@johnius.ru>
2016-06-27 11:54:54 -0400

    Async

--- app/async.js
+++ app/async.js
@@ -2,10 +2,12 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return Promise.resolve(value)
   },
 
   manipulateRemoteData: function(url) {
-
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => data.people.map(person => person.name).sort())
   }
 };
