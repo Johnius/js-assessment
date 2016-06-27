7d78987297368a65607a28d310f3e936bf3d4c4a
Johnius <me@johnius.ru>
2016-06-27 11:18:06 -0400

    FizzBuzz

--- app/flowControl.js
+++ app/flowControl.js
@@ -11,5 +11,22 @@ exports.flowControlAnswers = {
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
 
+    // Number should be either float or integer
+    return isNaN(parseFloat(num))
+      // If it's not true then return false
+      ? false
+      // But if num is number and it's 0 then return 0
+      // If it's not 0 then (&&) use Conditional (ternary) Operator to check
+      //   if the number is devisible by 3 and/or by 5.
+      // If it's devisible in one or both of the cases then return
+      //   Fizz and/or Buzz concatenated into string
+      // Otherwise these conditional operators in sum will return
+      //   empty string which then lead to evaluation of OR (||) logical
+      //   operator that returns initial number
+      : num && (num % 3 === 0 ? 'fizz' : '') + (num % 5 === 0 ? 'buzz' : '') || num
+
+    // This way is not the best one for clearly understand what is going on
+    //   but it shows my awareness of conditional and logical operators and
+    //   how they can be combined
   }
 };
